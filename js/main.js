175f284a2a43ca3be630f293e45fa93eec58df3f
reyesjerez <reyesjerezpe@gmail.com>
2023-12-09 10:09:52 +0100

    Add functions and conditionals

--- js/main.js
+++ js/main.js
@@ -3,27 +3,40 @@
 // query selectors
 const number = document.querySelector('.js__number');
 const button = document.querySelector('.js__button');
-const result1 = document.querySelector('.js__paragraph1');
-const result2 = document.querySelector('.js__paragraph2');
+let result1 = document.querySelector('.js__paragraph1');
+let result2 = document.querySelector('.js__paragraph2');
 
 
 // variables
+const randomNumber = getRandomNumber(100);
 
+const numberValue = parseFloat(number.value);
 
+console.log(number.value);
 
 // funciones
-
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
 
+function handleClickButton(event) {
+    event.preventDefault;
+    if (randomNumber === numberValue) {
+        console.log(`El número es igual`);
+    }
+    else if (randomNumber > numberValue) {
+        console.log(`El número es mayor`);
+    }
+    else if (randomNumber < numberValue) {
+        console.log(`El número es menor`); 
+    }
+}
 
 // eventos
-
+button.addEventListener('click', handleClickButton);
 
 
 // código que se ejecuta al cargar la página
-
-const randomNumber = getRandomNumber(100);
-
 console.log (`Mi número aleatorio es ${randomNumber}`)
+result1.innerHTML = `Pista: Escribe el número y dale a Prueba.`
+result2.innerHTML = `Número de intentos: `
